ddd4c90a4715dc8e5d3c89e2f78455e300be6820
monishramadoss <monish.ramadoss@outlook.com>
2021-07-11 23:37:16 -0700

    code update

--- covid_biomarkers/run.sh
+++ covid_biomarkers/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export JARVIS_PATH_CONFIGS=/home/mramados/.jarvis
 
-python /home/mramados/Pneumonia_Detector/dual_loss/model.py > /home/mramados/Pneumonia_Detector/dual_loss/stdout 2>&1
-python /home/mramados/Pneumonia_Detector/single_loss/model.py > /home/mramados/Pneumonia_Detector/single_loss/stdout 2>&1
+python /home/mramados/Pneumonia_Detector/covid_biomarkers/dual_loss/model.py > /home/mramados/Pneumonia_Detector/covid_biomarkers/dual_loss/stdout 2>&1
+python /home/mramados/Pneumonia_Detector/covid_biomarkers/single_loss/model.py > /home/mramados/Pneumonia_Detector/covid_biomarkers/single_loss/stdout 2>&1
