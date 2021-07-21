57411d53e27b863f8e9eb56aa5c2949fabc12a77
monishramadoss <monish.ramadoss@outlook.com>
2021-07-21 12:20:21 -0700

    refactor

--- covid_biomarkers/run_dual_loss.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export JARVIS_PATH_CONFIGS=/home/mramados/.jarvis
-python /home/mramados/Pneumonia_Detector/covid_biomarkers/dual_loss.py > /home/mramados/Pneumonia_Detector/covid_biomarkers/dual_loss_stdout 2>&1
--- covid_biomarkers/run_vit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export JARVIS_PATH_CONFIGS=/home/mramados/.jarvis
-python /home/mramados/Pneumonia_Detector/covid_biomarkers/vit_model.py > /home/mramados/Pneumonia_Detector/covid_biomarkers/vit_loss_stdout 2>&1
